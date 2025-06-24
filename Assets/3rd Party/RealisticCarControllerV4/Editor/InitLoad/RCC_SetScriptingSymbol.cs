dc34e1c9dcc594a4ba85f318b732fe6570572c5f
Andhika-Suryanto <andhikasuryanto.id@gmail.com>
2025-06-24 07:19:55 +0700

    [DEV] Adding RCC to Project and Customizing Forest Scene
    ## Changes Made
    - Added RCC Unity package for modifying cars
    - Fixed broken terrain in Forest
    - Fixed JSON in ASE package
    
    ## Development Notes
    - There is some broken code, then it has been fixed but still not tested what outcome will come out in the RCC.
    
    ## Modified Files
    - Assets/Scenes/Forest.unity (modified)
    - Assets/3rd Party/RealisticCarControllerV4 (new)
    - Assets/3rd Party/AmplifyShaderEditor (modified)

---
+++ Assets/3rd Party/RealisticCarControllerV4/Editor/InitLoad/RCC_SetScriptingSymbol.cs
@@ -0,0 +1,53 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2014 - 2025 BoneCracker Games
+// https://www.bonecrackergames.com
+// Ekrem Bugra Ozdoganlar
+//
+//----------------------------------------------
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RCC_SetScriptingSymbol {
+
+    public static void SetEnabled(string defineName, bool enable) {
+
+        foreach (BuildTarget buildTarget in Enum.GetValues(typeof(BuildTarget))) {
+
+            BuildTargetGroup group = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            if (group == BuildTargetGroup.Unknown)
+                continue;
+
+            // Gunakan NamedBuildTarget
+            NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(group);
+
+            // Ambil define symbols
+            var curDefineSymbols = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget)
+                                                 .Split(';')
+                                                 .Select(d => d.Trim())
+                                                 .ToList();
+
+            if (enable) {
+                if (!curDefineSymbols.Contains(defineName))
+                    curDefineSymbols.Add(defineName);
+            } else {
+                if (curDefineSymbols.Contains(defineName))
+                    curDefineSymbols.Remove(defineName);
+            }
+
+            try {
+                PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, string.Join(";", curDefineSymbols));
+            } catch (Exception e) {
+                Debug.Log($"Could not set {defineName} scripting define symbol for build target: {buildTarget}, group: {group}, error: {e}");
+            }
+        }
+
+    }
+
+}
